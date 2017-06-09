7e584e45f3bf20b373734e685ee3f87c45fe0038
Ubuntu <ralfpopescu@gmail.com>
2017-06-09 21:22:01 +0000

    we did it

--- spotifykafka.py
+++ spotifykafka.py
@@ -1,12 +1,32 @@
-from Kafka import KafkaConsumer
+from kafka import KafkaConsumer
 import os
 import json
-from flask import Flask, request, redirect, g, render_template
 import requests
 import base64
 import urllib
-from Kafka import KafkaProducer
+from kafka import KafkaProducer
 
-playlist_consumer = KafkaConsumer('playlists')
+r = requests.get('http://127.0.0.1:5000/access-token')
+token = r.text
 
-r = requests.get('127.0.0.1/access-token')
+headers = {"Authorization": "Bearer " + token, }
+r = requests.get('https://api.spotify.com/v1/me', headers=headers)
+user_data = json.loads(r.text)
+
+USER_ID = user_data.get('id')
+spotify_playlist = 'https://api.spotify.com/v1/users/' + USER_ID + '/playlists'
+
+playlist_consumer = KafkaConsumer('playlists', group_id="anystring")
+r = requests.get('http://127.0.0.1:5000/playlists')
+
+for msg in playlist_consumer:
+    try:
+        print(msg)
+        value = msg.value.decode('utf-8')
+        value = json.loads(value)
+        print(value)
+        r = requests.post(spotify_playlist, json=value, headers=headers)
+        print(r.text)
+    except Exception:
+        print('ERROR!')
+        print(msg)
--- spotifything.py
+++ spotifything.py
@@ -4,15 +4,19 @@ from flask import Flask, request, redirect, g, render_template
 import requests
 import base64
 import urllib
-from Kafka import KafkaProducer
+from kafka import KafkaProducer
 
 app = Flask(__name__)
 
 REDIRECT_URI = 'http://127.0.0.1:5000/redirect'
 SPOTIFY_API_URL = "https://api.spotify.com/v1"
-CLIENT_ID = os.environ["CLIENT_ID"]
-CLIENT_SECRET = os.environ["CLIENT_SECRET"]
-kafka_producer = KafkaProducer(bootstrap_servers='localhost:5000')
+# CLIENT_ID = os.environ["CLIENT_ID"]
+# CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+
+CLIENT_ID = "0b0b92a99b264a6da92ce78353994034"
+CLIENT_SECRET = "fcb8728571c748ecb8e47ac887c96dfc"
+
+kafka_producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
 @app.route('/')
 def requestAuth():
@@ -44,23 +48,23 @@ def requestAccess():
     with open('access_token.txt', 'w') as f:
         f.write(access_token)
 
-    return { 'message': 'Success!' }
+    return "hi"
 
 @app.route('/access-token')
 def get_access_token():
     with open('access_token.txt', 'r') as f:
         return f.read()
 
-@app.route('/playlists', methods=['POST'])
+@app.route('/playlists')
 def create_playlist():
-    r = requests.get('https://api.spotify.com/v1/me')
-    user_data = json.loads(r.text)
-    USER_ID = user_data.get('id')   spotify_playlist = 'https://api.spotify.com/v1/users/' + USER_ID + '/playlists'
     message = {
         "description": "New playlist description",
-        "public": false,
+        "public": False,
         "name": "New Playlist"
         }
+    message = json.dumps(message)
+    kafka_producer.send('playlists', message.encode('utf-8'))
+    return "hello"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
